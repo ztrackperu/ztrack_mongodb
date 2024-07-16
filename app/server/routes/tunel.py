cf86e155075e34b9065f8710c195eafd5e88dceb
ztrackperu <ztrack@zgroup.com.pe>
2024-07-16 12:34:01 -0500

    Base para tunel hortifruit 1

--- app/server/routes/tunel.py
+++ app/server/routes/tunel.py
@@ -5,7 +5,7 @@ from fastapi.responses import JSONResponse
 
 #aqui pedimos las funciones que incluyen nuestro CRUD
 from server.funciones.tunel import (
-    data_madurador,
+    data_hortifruit,
 
     
 )
@@ -17,5 +17,16 @@ from server.models.tunel import (
 
 
 )
+
+
 #aqui se definen las rutas de la API REST
 router = APIRouter()
+
+
+@router.post("/Hortifruit/", response_description="Datos del tunel agregados a la base de datos.")
+#La funcion espera "ConceptoOTSchema"
+async def add_tunel_hortifruit_data(notificacion: TunelSchema = Body(...)):
+    #convertir en json
+    notificacion = jsonable_encoder(notificacion)   
+    new_notificacion = await data_hortifruit(notificacion)
+    return ResponseModel(new_notificacion, "ok")
