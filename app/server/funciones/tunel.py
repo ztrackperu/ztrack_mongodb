1b5b858092ae0484f2e3d518ab0d6cba3812a75c
ztrackperu <ztrack@zgroup.com.pe>
2024-07-18 11:48:03 -0500

    Base para tunel hortifruit 5

--- app/server/funciones/tunel.py
+++ app/server/funciones/tunel.py
@@ -23,6 +23,129 @@ def obtener_mes_y_anio_actual():
     mes_anio_str = f"{mes_actual}_{anio_actual}"
     return mes_anio_str
 
+def validarON(cadena):
+    # Validar si la cadena es numérica
+    try:
+        valor_numerico = float(cadena)
+        return float(valor_numerico)
+    except ValueError:
+        pass
+    
+    # Si la cadena no es numérica, verificar si comienza con "TD:"
+    if cadena.startswith("TD:"):
+        # Quitar "TD:" del principio y retornar el resto de la cadena
+        return cadena[3:]
+    else:
+        # Retornar 'NA' si no es numérica y no comienza con "TD:"
+        return 'NA'
+
+def validar_dato(dato):
+    if isinstance(dato, str):
+        dato = dato.strip()  # Eliminamos espacios en blanco al inicio y al final
+    
+    if isinstance(dato, str) and dato.lower() == 'na':
+        return None
+    try:
+        return float(dato)
+    except ValueError:
+        return None
+
+def procesaObjeto(trama,idProgre,fecha_wonderful,tele_wonderful):
+    vali = trama.split(',')
+    objetoV = {
+        "id": idProgre,
+        "set_point": validar_dato(vali[3]), 
+        "temp_supply_1": validar_dato(vali[4]),
+        "temp_supply_2": validar_dato(vali[5]),
+        "return_air": validar_dato(vali[6]), 
+        "evaporation_coil": validar_dato(vali[7]),
+        "condensation_coil": validar_dato(vali[8]),
+        "compress_coil_1": validar_dato(vali[9]),
+        "compress_coil_2": validar_dato(vali[10]), 
+        "ambient_air": validar_dato(vali[11]), 
+        "cargo_1_temp": validar_dato(vali[12]),
+        "cargo_2_temp": validar_dato(vali[13]), 
+        "cargo_3_temp": validar_dato(vali[14]), 
+        "cargo_4_temp": validar_dato(vali[15]), 
+        "relative_humidity": validar_dato(vali[16]), 
+        "avl": validar_dato(vali[17]), 
+        "suction_pressure": validar_dato(vali[18]), 
+        "discharge_pressure": validar_dato(vali[19]), 
+        "line_voltage": validar_dato(vali[20]), 
+        "line_frequency": validar_dato(vali[21]), 
+        "consumption_ph_1": validar_dato(vali[22]), 
+        "consumption_ph_2": validar_dato(vali[23]), 
+        "consumption_ph_3": validar_dato(vali[24]), 
+        "co2_reading": validar_dato(vali[25]), 
+        "o2_reading": validar_dato(vali[26]), 
+        "evaporator_speed": validar_dato(vali[27]), 
+        "condenser_speed": validar_dato(vali[28]),
+        "power_kwh": validar_dato(vali[29]),
+        "power_trip_reading": validar_dato(vali[30]),
+        "suction_temp": validar_dato(vali[31]),
+        "discharge_temp": validar_dato(vali[32]),
+        "supply_air_temp": validar_dato(vali[33]),
+        "return_air_temp": validar_dato(vali[34]),
+        "dl_battery_temp": validar_dato(vali[35]),
+        "dl_battery_charge": validar_dato(vali[36]),
+        "power_consumption": validar_dato(vali[37]),
+        "power_consumption_avg": validar_dato(vali[38]),
+        "alarm_present": validar_dato(vali[39]),
+        "capacity_load": validar_dato(vali[40]),
+        "power_state": validar_dato(validarON(vali[41])), 
+        "controlling_mode": validar_dato(vali[42]),
+        "humidity_control": validar_dato(vali[43]),
+        "humidity_set_point": validar_dato(vali[44]),
+        "fresh_air_ex_mode": validar_dato(vali[45]),
+        "fresh_air_ex_rate": validar_dato(vali[46]),
+        "fresh_air_ex_delay": validar_dato(vali[47]),
+        "set_point_o2": validar_dato(vali[48]),
+        "set_point_co2": validar_dato(vali[49]),
+        "defrost_term_temp": validar_dato(vali[50]),
+        "defrost_interval": validar_dato(vali[51]),
+        "water_cooled_conde": validar_dato(vali[52]),
+        "usda_trip": validar_dato(vali[53]),
+        "evaporator_exp_valve": validar_dato(vali[54]),
+        "suction_mod_valve": validar_dato(vali[55]),
+        "hot_gas_valve": validar_dato(vali[56]),
+        "economizer_valve": validar_dato(vali[57]),
+        "ethylene": validar_dato(vali[58]),
+        "stateProcess": validar_dato(vali[59]),
+        "stateInyection": validar_dato(vali[60]),
+        "timerOfProcess": validar_dato(vali[61]),
+        "battery_voltage": validar_dato(vali[62]),
+        "power_trip_duration":validar_dato(vali[63]),
+        "modelo": validar_dato(vali[64]),
+        "latitud": 35.7396,
+        "longitud":  -119.238,
+        "created_at": fecha_wonderful,
+        "telemetria_id": tele_wonderful,
+        "inyeccion_etileno": 0,
+        "defrost_prueba": 0,
+        "ripener_prueba": 0,
+        "sp_ethyleno": validar_dato(vali[67]),
+        "inyeccion_hora": validar_dato(vali[68]),
+        "inyeccion_pwm": validar_dato(vali[69]),
+        "extra_1": 0,
+        "extra_2": 0,
+        "extra_3": 0,
+        "extra_4": 0,
+        "extra_5": 0
+    }
+    return objetoV
+
+
+def generar_cadena_extendida(cadena_original, nuevas_posiciones, numero_elementos):
+    # Convertimos la cadena original en una lista separada por comas
+    elementos = cadena_original.split(',')  
+    # Creamos una lista inicial con 'NA' en todas las posiciones según el número de elementos deseado
+    resultado = ['NA'] * numero_elementos
+    # Insertamos los elementos en las nuevas posiciones especificadas
+    for posicion, elemento in zip(nuevas_posiciones, elementos):
+        resultado[posicion - 1] = elemento  # Restamos 1 porque las posiciones son 1-indexed
+    # Convertimos la lista resultado de nuevo a cadena separada por comas y la retornamos
+    return ','.join(resultado)
+
 async def data_hortifruit(notificacion_data: dict) -> dict:
     #capturar hora 
     #desglozar la data y almacenar en base de datos REPOSITORIO_MES_AÑO
@@ -48,6 +171,15 @@ async def data_hortifruit(notificacion_data: dict) -> dict:
 #procesamiento de informacion 
 
 
+# Ejemplo de uso:
+#cadena_original = "COD,PAR,POR,TRA,JOB,TEL,SPU"
+#nuevas_posiciones = [1, 4, 7, 8, 13, 21, 22]
+#numero_elementos = 25
+
+#resultado = generar_cadena_extendida(cadena_original, nuevas_posiciones, numero_elementos)
+#print(resultado)
+
+
 async def homologar_hortifruit_123321() -> dict:
     datazo = obtener_mes_y_anio_actual()
     baseD = "HORTIFRUIT_"+datazo
@@ -66,147 +198,27 @@ async def homologar_hortifruit_123321() -> dict:
     proceso =0
     fecha_anterior=None
     async for x in collectionMongo.find({"status":1}).sort("fecha",1):
+        cad =x['data']
+        nuevas_posiciones = [65,2,1,3,42,13,14,15,16,5,7,4,40]
+        numero_elementos = 70
+        fecha_dato=x['fecha']
+        tele_dato=25800 
+        nova_cadena = generar_cadena_extendida(cad, nuevas_posiciones, numero_elementos)
+        print(nova_cadena)
+        objeto_generado = procesaObjeto(nova_cadena,idProgre,fecha_dato,tele_dato)
+        print(objeto_generado)
+
         if proceso==0:
             fecha_anterior=x['fecha']
+            databaseMongoH = client['ztrack_ja']  
+            collectionMongoH = databaseMongoH.get_collection("madurador")
             print('insertar')
             proceso=1
         else:
             #dato proceso de fecha
             if (x['fecha']-fecha_anterior)>30 :
+                databaseMongoH = client['homologado_ecuador']  
+                collectionMongoH = databaseMongoH.get_collection("123321")
                 print('insertar')
             proceso=0
-            
-        cad =x['data']
-        #SECTORIZAR LA TRAMA PARA UNIRLA 
-        f1 =cad.split('|')
-        p1=f1[0]
-        f2=p1.split(',')
-        print(cad)
-        if f2[0]=='1CR1':
-            trama=''
-            trama =trama+f1[0]
-            p2=f1[1]
-            datote =p2.split('*')
-            trama=trama+datote[0]
-            print('vamo construyendo 1')
-        if f2[0]=='1CR2':
-            if trama!='':
-                p2=f1[1]
-                datote =p2.split('*')
-                trama=trama+datote[0]
-                print('vamo construyendo 2')
-        if f2[0]=='1CR3':
-            if trama!='':
-                idProgre=idProgre+1
-                p2=f1[1]
-                datote =p2.split('*')
-                trama=trama+datote[0]
-                print(trama)
-                vali = trama.split(',')
-                if len(vali)==70:
-                    print("listo pa insertar")
-                    #aqui realizar la insercion de datos 
-                    fecha_wonderful=x['fecha']
-                    #telemetria_id de los dispositivos
-                    #ZGRU1090804 -> 33
-                    #ZGRU2009227 -> 259
-                    #ZGRU2008220 -> 260
-                    #ZGRU2232647 -> 258
-                    tele_wonderful=258                    
-
-                    objetoV = {
-                        "id": idProgre,
-                        "set_point": float(vali[3]), 
-                        "temp_supply_1": float(vali[4]),
-                        "temp_supply_2": float(vali[5]),
-                        "return_air": float(vali[6]), 
-                        "evaporation_coil": float(vali[7]),
-                        "condensation_coil": float(vali[8]),
-                        "compress_coil_1": float(vali[9]),
-                        "compress_coil_2": float(vali[10]), 
-                        "ambient_air": float(vali[11]), 
-                        "cargo_1_temp": float(vali[12]),
-                        "cargo_2_temp": float(vali[13]), 
-                        "cargo_3_temp": float(vali[14]), 
-                        "cargo_4_temp": float(vali[15]), 
-                        "relative_humidity": float(vali[16]), 
-                        "avl": float(vali[17]), 
-                        "suction_pressure": float(vali[18]), 
-                        "discharge_pressure": float(vali[19]), 
-                        "line_voltage": float(vali[20]), 
-                        "line_frequency": float(vali[21]), 
-                        "consumption_ph_1": float(vali[22]), 
-                        "consumption_ph_2": float(vali[23]), 
-                        "consumption_ph_3": float(vali[24]), 
-                        "co2_reading": float(vali[25]), 
-                        "o2_reading": float(vali[26]), 
-                        "evaporator_speed": float(vali[27]), 
-                        "condenser_speed": float(vali[28]),
-                        "power_kwh": float(vali[29]),
-                        "power_trip_reading": float(vali[30]),
-                        
-                        "suction_temp": float(vali[31]),
-                        "discharge_temp": float(vali[32]),
-                        "supply_air_temp": float(vali[33]),
-                        "return_air_temp": float(vali[34]),
-                        "dl_battery_temp": float(vali[35]),
-                        "dl_battery_charge": float(vali[36]),
-                        "power_consumption": float(vali[37]),
-                        "power_consumption_avg": float(vali[38]),
-                        "alarm_present": float(vali[39]),
-                        "capacity_load": float(vali[40]),
-                        "power_state": float(vali[41]), 
-                        "controlling_mode": vali[42],
-                        "humidity_control": float(vali[43]),
-                        "humidity_set_point": float(vali[44]),
-                        "fresh_air_ex_mode": float(vali[45]),
-                        "fresh_air_ex_rate": float(vali[46]),
-                        "fresh_air_ex_delay": float(vali[47]),
-                        "set_point_o2": float(vali[48]),
-                        "set_point_co2": float(vali[49]),
-                        "defrost_term_temp": float(vali[50]),
-                        "defrost_interval": float(vali[51]),
-                        "water_cooled_conde": float(vali[52]),
-                        "usda_trip": float(vali[53]),
-                        "evaporator_exp_valve": float(vali[54]),
-                        "suction_mod_valve": float(vali[55]),
-                        "hot_gas_valve": float(vali[56]),
-                        "economizer_valve": float(vali[57]),
-                        "ethylene": float(vali[58]),
-                        "stateProcess": vali[59],
-                        "stateInyection": vali[60],
-                        "timerOfProcess": float(vali[61]),
-                        "battery_voltage": float(vali[62]),
-                        "power_trip_duration":float(vali[63]),
-                        "modelo": vali[64],
-                        "latitud": 35.7396,
-                        "longitud":  -119.238,
-                        "created_at": fecha_wonderful,
-                        "telemetria_id": tele_wonderful,
-                        "inyeccion_etileno": 0,
-                        "defrost_prueba": 0,
-                        "ripener_prueba": 0,
-                        "sp_ethyleno": int(vali[67]),
-                        "inyeccion_hora": int(vali[68]),
-                        "inyeccion_pwm": float(vali[69]),
-                        "extra_1": 0,
-                        "extra_2": 0,
-                        "extra_3": 0,
-                        "extra_4": 0,
-                        "extra_5": 0
-                    }
-
-                    #actualizar en base de mysql
-                    #insertaren bd mongodb 
-                    #crear dat h_ZGRU1090804
-                    #ZGRU1090804 -> 33
-                    #ZGRU2009227 -> 259
-                    #ZGRU2008220 -> 260
-                    #ZGRU2232647 -> 258
-                    #databaseMongoH = client['Homologar']  
-                    #collectionMongoH = databaseMongoH.get_collection("ZGRU2009227")
-                    databaseMongoH = client['ztrack_ja']  
-                    collectionMongoH = databaseMongoH.get_collection("madurador")
-                    #collectionMongoH.insert_one(objetoV)
-
     return baseD
