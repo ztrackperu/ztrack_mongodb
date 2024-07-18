9ef8f3b58529fde87992b00ab6e256ff931c2d6c
ztrackperu <ztrack@zgroup.com.pe>
2024-07-18 14:01:15 -0500

    Base para tunel hortifruit 13

--- app/server/funciones/tunel.py
+++ app/server/funciones/tunel.py
@@ -54,19 +54,19 @@ def procesaObjeto(trama,idProgre,fecha_wonderful,tele_wonderful):
     vali = trama.split(',')
     objetoV = {
         "id": idProgre,
-        "set_point": validar_dato(validar_numero(vali[3])), 
-        "temp_supply_1": validar_dato(validar_numero(vali[4])),
+        "set_point": validar_numero(validar_dato(vali[3])), 
+        "temp_supply_1": validar_numero(validar_dato(vali[4])),
         "temp_supply_2": validar_dato(vali[5]),
-        "return_air": validar_dato(validar_numero(vali[6])), 
+        "return_air": validar_numero(validar_dato(vali[6])), 
         "evaporation_coil": validar_dato(vali[7]),
         "condensation_coil": validar_dato(vali[8]),
         "compress_coil_1": validar_dato(vali[9]),
         "compress_coil_2": validar_dato(vali[10]), 
         "ambient_air": validar_dato(vali[11]), 
-        "cargo_1_temp": validar_dato(validar_numero(vali[12])),
-        "cargo_2_temp": validar_dato(validar_numero(vali[13])), 
-        "cargo_3_temp": validar_dato(validar_numero(vali[14])), 
-        "cargo_4_temp": validar_dato(validar_numero(vali[15])), 
+        "cargo_1_temp": validar_numero(validar_dato(vali[12])),
+        "cargo_2_temp": validar_numero(validar_dato(vali[13])), 
+        "cargo_3_temp": validar_numero(validar_dato(vali[14])), 
+        "cargo_4_temp": validar_numero(validar_dato(vali[15])), 
         "relative_humidity": validar_dato(vali[16]), 
         "avl": validar_dato(vali[17]), 
         "suction_pressure": validar_dato(vali[18]), 
@@ -177,9 +177,9 @@ def validar_numero(dato):
         if dato==-38.5 or dato==-51.2:
             return dato
         else :
-            return 'NA'
+            return None
     else:
-        return 'NA'
+        return None
     
 async def data_hortifruit(notificacion_data: dict) -> dict:
     #capturar hora 
