8e0d2822b186969d0fbcaa39c00da1f7a8546620
ztrackperu <ztrack@zgroup.com.pe>
2024-07-19 18:32:08 -0500

    Base para tunel hortifruit 37

--- app/server/funciones/tunel.py
+++ app/server/funciones/tunel.py
@@ -248,7 +248,7 @@ async def homologar_hortifruit_123321() -> dict:
     proceso =0
     fecha_anterior=None
     #Session=Depends(get_db)
-    datos = await get_user(Depends(get_db),471)
+    datos = await get_user(Session=Depends(get_db),user_id=471)
     #datos = await get_user(Session=Depends(get_db),471)
     print(datos)
     async for x in collectionMongo.find({"status":10}).sort("fecha",1):
