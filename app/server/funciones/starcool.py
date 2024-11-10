80d21807c70b17cb617202d0a983d96a59fa0267
ztrackperu <ztrack@zgroup.com.pe>
2024-11-10 17:15:15 -0500

    homologar starcool general 12

--- app/server/funciones/starcool.py
+++ app/server/funciones/starcool.py
@@ -23,6 +23,54 @@ def obtener_mes_ano_anterior():
     repositorio = f"REPOSITORIO_{fecha_anterior.strftime('%m')}_{fecha_anterior.strftime('%Y')}"
     return repositorio
 
+#ARRAY DONDE SE DEBE CARGAR LOS DATOS:
+cmd=[22,22,147,249,195,250,219,251,7,251,3,197,147,250,219,224,3,224,3,224]
+
+def Calcula_Termino1(num1,num2):
+    valve = 0xFF
+    valve = valve & num1
+    valve = valve << 8
+    valve = valve & 0xFFFF
+    valve = valve | num2
+    #FUNCION CALCULA SP    
+    aux = valve
+    signo = 0
+    aux = aux & 0xF000
+    if(aux == 0xF000):
+        signo = 1
+        valve = valve - 0xF000
+    else:
+        valve = valve - 0xC000
+    valve = valve >> 4
+    if(signo==1):
+        valve = 0x100 - valve
+    valve = valve / 4.0
+    if(signo == 1):
+        valve = -valve
+    return valve
+
+#VALOR DE SETPOINT
+
+def retorna_bloque_decodificado(cmd_3,cmd_4,cmd_5,cmd_6,cmd_7,cmd_8,cmd_9,cmd_10):
+    sp = Calcula_Termino1(cmd_3,cmd_4)
+    #print("Valor SP: ")
+    #print(sp)
+    #VALOR DE SUMINISTRO
+    supply = Calcula_Termino1(cmd_5,cmd_6)
+    #print("Valor SUM: ")
+    #print(sp)
+    #VALOR DE RETORNO
+    ret = Calcula_Termino1(cmd_7,cmd_8)
+    #print("Valor RET: ")
+    #print(ret)
+    #VALOR DE EVAP
+    evap = Calcula_Termino1(cmd_9,cmd_10)
+    #print("Valor EVAP: ")
+    #print(evap)
+    return [sp,supply,ret,evap]
+
+
+
 async def homologar_starcool_general() -> dict:
     datazo = BaseConexion.obtener_mes_y_anio_actual()
     baseD = "REPOSITORIO_"+datazo
@@ -78,27 +126,131 @@ async def homologar_starcool_general() -> dict:
             print(estadoC)
             print(idProgre)
             collectionMongo = databaseMongo.get_collection("tunel")
-            #async for x in collectionMongo.find(factorBusqueda).sort("fecha",1):
-                #cad =x['data']
-                #fechaA=x['fecha']
+            async for x in collectionMongo.find(factorBusqueda).sort("fecha",1):
+                cad =x['data']
+                fechaA=x['fecha']
                 #SECTORIZAR LA TRAMA PARA UNIRLA 
-                #f2 =cad.split(',')
+                f2 =cad.split(',')
                 #aqui empieza la homologazion
-                #if f2[2]=='STARCOOL01':
-                    #print("oli")
-                
-
-
-
-            
-
-
-
-
-
-
+                if f2[2]==datos_contenedor_acumulados[0][1]:
+                    #pasar por el algoritmo de procesamiento de informacion y homologar 
+                    #1TC2,Madurador,ZGRU6844452,22,22,147,249,195,249,115,249,195,250,11,197,67,249,119,224,3,224,3,224,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0,0.00,0,0,0,0,0,0.00,0.00,0.00,0.00,0.00,0.00,0,0,0.00,0.00,0.00,0.00,0.00,0,0,0,0.00,0.00,THERMOKING,0.000,0.000,0,0,0
+                    if len(f2)>69 :
+                        datos_homologados = retorna_bloque_decodificado(f2[3],f2[4],f2[5],f2[6],f2[7],f2[8],f2[9],f2[10])
+                        #[sp,supply,ret,evap]
+                        if len(datos_homologados)==4:
+                            objetoV = {
+                                "id": idProgre,
+                                "set_point": datos_homologados[0], 
+                                "temp_supply_1": datos_homologados[1],
+                                "temp_supply_2": BaseConexion.convertir_a_float(f2[17]),
+                                "return_air": datos_homologados[2], 
+                                "evaporation_coil": datos_homologados[3],
+                                "condensation_coil": BaseConexion.convertir_a_float(f2[18]),
+                                "compress_coil_1": None,
+                                "compress_coil_2": BaseConexion.convertir_a_float(f2[20]), 
+                                "ambient_air": BaseConexion.convertir_a_float(f2[21]), 
+                                "cargo_1_temp": BaseConexion.convertir_a_float(f2[22]),
+                                "cargo_2_temp": BaseConexion.convertir_a_float(f2[23]), 
+                                "cargo_3_temp": BaseConexion.convertir_a_float(f2[24]), 
+                                "cargo_4_temp": BaseConexion.convertir_a_float(f2[25]), 
+                                "relative_humidity": None, 
+                                "avl": BaseConexion.convertir_a_float(f2[17]), 
+                                "suction_pressure": BaseConexion.convertir_a_float(f2[18]), 
+                                "discharge_pressure": BaseConexion.convertir_a_float(f2[19]), 
+                                "line_voltage": BaseConexion.convertir_a_float(f2[20]), 
+                                "line_frequency": BaseConexion.convertir_a_float(f2[21]), 
+                                "consumption_ph_1": BaseConexion.convertir_a_float(f2[22]), 
+                                "consumption_ph_2": BaseConexion.convertir_a_float(f2[23]), 
+                                "consumption_ph_3": BaseConexion.convertir_a_float(f2[24]), 
+                                "co2_reading": BaseConexion.convertir_a_float(f2[25]), 
+                                "o2_reading": BaseConexion.convertir_a_float(f2[26]), 
+                                "evaporator_speed": BaseConexion.convertir_a_float(f2[27]), 
+                                "condenser_speed": BaseConexion.convertir_a_float(f2[28]),
+                                "power_kwh": BaseConexion.convertir_a_float(f2[29]),
+                                "power_trip_reading": BaseConexion.convertir_a_float(f2[30]),
+                                "suction_temp": BaseConexion.convertir_a_float(f2[31]),
+                                "discharge_temp": BaseConexion.convertir_a_float(f2[32]),
+                                "supply_air_temp": BaseConexion.convertir_a_float(f2[33]),
+                                "return_air_temp": BaseConexion.convertir_a_float(f2[34]),
+                                "dl_battery_temp": BaseConexion.convertir_a_float(f2[35]),
+                                "dl_battery_charge": BaseConexion.convertir_a_float(f2[36]),
+                                "power_consumption": BaseConexion.convertir_a_float(f2[37]),
+                                "power_consumption_avg": BaseConexion.convertir_a_float(f2[38]),
+                                "alarm_present": BaseConexion.convertir_a_float(f2[39]),
+                                "capacity_load": BaseConexion.convertir_a_float(f2[40]),
+                                "power_state": 1, 
+                                "controlling_mode": f2[42],
+                                "humidity_control": BaseConexion.convertir_a_float(f2[43]),
+                                "humidity_set_point": BaseConexion.convertir_a_float(f2[44]),
+                                "fresh_air_ex_mode": BaseConexion.convertir_a_float(f2[45]),
+                                "fresh_air_ex_rate": BaseConexion.convertir_a_float(f2[46]),
+                                "fresh_air_ex_delay": BaseConexion.convertir_a_float(f2[47]),
+                                "set_point_o2": BaseConexion.convertir_a_float(f2[48]),
+                                "set_point_co2": BaseConexion.convertir_a_float(f2[49]),
+                                "defrost_term_temp": BaseConexion.convertir_a_float(f2[50]),
+                                "defrost_interval": BaseConexion.convertir_a_float(f2[51]),
+                                "water_cooled_conde": BaseConexion.convertir_a_float(f2[52]),
+                                "usda_trip": BaseConexion.convertir_a_float(f2[53]),
+                                "evaporator_exp_valve": BaseConexion.convertir_a_float(f2[54]),
+                                "suction_mod_valve": BaseConexion.convertir_a_float(f2[55]),
+                                "hot_gas_valve": BaseConexion.convertir_a_float(f2[56]),
+                                "economizer_valve": BaseConexion.convertir_a_float(f2[57]),
+                                "ethylene": BaseConexion.convertir_a_float(f2[58]),
+                                "stateProcess": f2[59],
+                                "stateInyection": f2[60],
+                                "timerOfProcess": BaseConexion.convertir_a_float(f2[61]),
+                                "battery_voltage": BaseConexion.convertir_a_float(f2[62]),
+                                "power_trip_duration":BaseConexion.convertir_a_float(f2[63]),
+                                "modelo": f2[17],
+                                "latitud": f2[17],
+                                "longitud":  f2[17],
+                                "created_at": fechaA,
+                                "telemetria_id": datos_contenedor_acumulados[0][9],
+                                "inyeccion_etileno": 0,
+                                "defrost_prueba": 0,
+                                "ripener_prueba": 0,
+                                "sp_ethyleno": BaseConexion.convertir_a_float(f2[67]),
+                                "inyeccion_hora": BaseConexion.convertir_a_float(f2[68]),
+                                "inyeccion_pwm": BaseConexion.convertir_a_float(f2[69]),
+                                "extra_1": 0,
+                                "extra_2": 0,
+                                "extra_3": 0,
+                                "extra_4": 0,
+                                "extra_5": 0
+                            }
+                            databaseMongoH = client['ztrack_ja']  
+                            collectionMongoH = databaseMongoH.get_collection("madurador_starcool")
+                            collectionMongoH.insert_one(objetoV)
+                            objetoControl ={
+                                "id":idProgre,
+                                "telemetria_id":objetoV['telemetria_id'],
+                                "fecha":fechaA
+                            }
+                            if estadoC==1 :
+                                #actualizar
+                                collectionControl.update_one({"telemetria_id":objetoV['telemetria_id'],},{"$set": {"id":idProgre,"fecha":fechaA}})
+                            else :
+                                #grabar
+                                collectionControl.insert_one(objetoControl)
+                                estadoC=1
+                            
+                            curB = cnx.cursor()
+                            update_old_salary = (
+                            "UPDATE contenedores SET ultima_fecha = %s ,set_point = %s ,temp_supply_1= %s ,return_air= %s"
+                            ", ambient_air= %s ,relative_humidity= %s ,avl = %s , defrost_prueba = %s , ripener_prueba = %s , ethylene = %s"
+                                " , set_point_co2 = %s , co2_reading = %s , humidity_set_point = %s , sp_ethyleno = %s , compress_coil_1 = %s WHERE estado = 1 AND telemetria_id = %s  ")
+                            curB.execute(update_old_salary, (fechaA, objetoV['set_point'],objetoV['temp_supply_1'], 
+                                                                objetoV['return_air'], objetoV['ambient_air'], objetoV['relative_humidity'], 
+                                                                objetoV['avl'], objetoV['inyeccion_pwm'], objetoV['inyeccion_hora'], 
+                                                                objetoV['ethylene'], objetoV['set_point_co2'], objetoV['co2_reading'], 
+                                                                objetoV['humidity_set_point'], objetoV['sp_ethyleno'],objetoV['compress_coil_1'], 
+                                                                objetoV['telemetria_id'],  ))
+                            cnx.commit()
         else :
             print("sin resultado")
+    cnx.close()
+
 
 
 
