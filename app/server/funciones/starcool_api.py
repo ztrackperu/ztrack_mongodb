bc1eeabadb6b92670cf0ea8e65f9e9e261bd2c19
lpnoobx <lpnoobx@gmail.com>
2024-12-09 18:31:04 -0500

    nestle ok 8

--- app/server/funciones/starcool_api.py
+++ app/server/funciones/starcool_api.py
@@ -193,8 +193,8 @@ async def homologar_api_starcool_general() -> dict:
         base_imei =obtener_mes_ano_anterior_imei(str(x['imei']))
         collection_especifica =databaseMongo.get_collection(base_imei)
         prueba_collection =databaseMongo.get_collection("prueba_colect")
-        ij = 170000
-        fecha_t ="2024-12-09T16:47:54"
+        ij = 175000
+        fecha_t ="2024-12-09T18:30:54"
         fecha_ok = datetime.fromisoformat(fecha_t)+timedelta(minutes=0)
         factorBusqueda ={"fecha":{"$gt":fecha_ok}}
         async for notificacion in collection_especifica.find(factorBusqueda,{"_id":0}).sort({"fecha":1}):
